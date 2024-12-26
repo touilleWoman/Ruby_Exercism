5d276841fd4e67234156a7599dd38353e644c474
touilleWoman <jie.zhou.ac@gmail.com>
2024-12-26 22:33:19 +0100

    add exercise acronym

---
+++ acronym/acronym.rb
@@ -0,0 +1,13 @@
+=begin
+Write your code for the 'Acronym' exercise in this file. Make the tests in
+`acronym_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/acronym` directory.
+=end
+module Acronym
+  def self.abbreviate(line)
+    words = line.scan(/\w+/)
+    words.map{|word| word[0].upcase}.join
+  end
+end
